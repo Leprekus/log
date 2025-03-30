4cfe044bed96054956dc2689e8895cb7e02fd4ff
root <root@DESKTOP-ERH9GVB.>
2025-03-30 16:21:54 -0700

    make bank scrollable

--- components/exercise_bank_form.tsx
+++ components/exercise_bank_form.tsx
@@ -18,7 +18,8 @@ export default function ExerciseBankForm({ exercise_bank }: ExerciseBankFormProp
 		dispatch_save_exercise(exercises);
 	}
 	return (
-	<form onSubmit={submit_handler}> <div className="grid gap-4 py-4">
+	<form onSubmit={submit_handler}> 
+	<div className="grid gap-4 py-4 max-h-[425px] overflow-auto mb-2">
 		{ exercise_bank.length < 1 ?
 			<DialogDescription> No exercises Found </DialogDescription> :
 		  exercise_bank === null ? 
