ca2c61d4c29b99601480fcf29903a562c6dd3111
leprekus <leprekus@yahoo.com>
2025-04-01 20:32:26 -0700

    made some changes dont remember

--- components/exercise_bank_form.tsx
+++ components/exercise_bank_form.tsx
@@ -7,6 +7,7 @@ import { Exercise } from "@/app/queries.actions";
 import { FormEvent } from "react";
 import { save_exercises, useTemplateStore } from "@/hooks/useTemplateStore";
 import { Checkbox } from "./ui/checkbox";
+import { Check } from "lucide-react";
 
 interface ExerciseBankFormProps { exercise_bank: Exercise[] };
 export default function ExerciseBankForm({ exercise_bank }: ExerciseBankFormProps) {
@@ -21,13 +22,14 @@ export default function ExerciseBankForm({ exercise_bank }: ExerciseBankFormProp
 	//TODO: implement retry fn
 	return (
 	<form onSubmit={submit_handler}> 
-	<div className="grid gap-4 py-4 max-h-[425px] overflow-auto mb-2">
+	<div className="grid gap-3 py-4 max-h-[50vh] overflow-auto mb-2">
 		{ exercise_bank.length < 1 ?
 			<DialogDescription> No exercises Found </DialogDescription> :
 		  exercise_bank === null ? 
 		  	<div> Could not load. <button> Retry </button></div> :
 		  exercise_bank.map( e => <Item key={e.name+e.exerciseid} title={e.name} id={e.exerciseid}>
-					  	<Checkbox id={e.exerciseid} name={e.name} value={e.exerciseid}/>
+					  	<Checkbox id={e.exerciseid} className="opacity-0" name={e.name} value={e.exerciseid}/>
+					  	{/*TODO: add check icon <Check className="text-muted-foreground"/>*/}
 		 			  </Item>)
 		}	
         </div>
--- components/item.tsx
+++ components/item.tsx
@@ -6,7 +6,7 @@ import { ReactNode } from "react";
 // the id in checkbox is used to propagate a click on label to select the checkbox
 interface ItemProps { title : string, id: string, children?: ReactNode };
 export default function Item({ title, id, children }: ItemProps) {
-	return <label htmlFor={id} className="w-full hover:bg-accent transition-all py-3 px-2 rounded-md  gap-4 grid grid-cols-3 items-center">
+	return <label htmlFor={id} className="w-full hover:bg-accent transition-all py-3 px-2 rounded-md  gap-4 grid grid-cols-3 items-center has-checked:bg-accent">
 			<input type="text" className="
 			font-semibold focus:outline-none focus:ring-0
 			hover:outline-none hover:ring-0 cursor-default
